7377792b33a3e9046f721cf882230a20a7b0d546
Iradukunda Ntabwoba Isaac <55135826+iraasaac@users.noreply.github.com>
2022-07-15 11:49:37 +0200

    new changes

---
+++ js/test.js
@@ -0,0 +1,11 @@
+   //variables
+   var fname ="Jean Paul";
+   var lname = "KANYABUNYOBWA"
+   var salary= 2000;
+   var vat=salary*0.18
+   var net = salary -(salary*vat)
+   //displlay element
+   document.write(fname+" "+lname)
+   document.write("<h1> Hello from javascript</h1>")
+   console.log("hello from javascript")
+   
